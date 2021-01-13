5f9cfce2a6d3c73e8e1872508170a39417c62f31
jab <joseadrianbuctuanon95@gmail.com>
2021-01-14 04:10:17 +0800

    updated slider

--- src/js/project/page_home.js
+++ src/js/project/page_home.js
@@ -47,7 +47,68 @@ function testimonialSection() {
     })
 }
 
+function doPlateSlider() {
+    const $gummies = $('.plate-anim__gummies')
+    const $hemp = $('.plate-anim__hemp')
+    const $border = $('.plate-anim__border')
+
+    const plateWidth = $('#plate-anim').width()
+
+    // set images first
+    console.log('plateWidth === ', plateWidth)
+    $gummies.find('img').css({ width: `${plateWidth}px` })
+    $hemp.find('img').css({ width: `${plateWidth}px` })
+
+    const $triggerEl = $('#about-cbd-section > .grid-container')
+
+
+
+    const plateAnimation = new TimelineMax()
+        //clip hemp to the left
+        .add(TweenMax.fromTo(
+            $hemp, 1,
+            {
+                width: '50%',
+            },
+            {
+                width: '0%',
+            }),0
+        )
+        //open gummies to the left
+        .add(TweenMax.fromTo(
+            $gummies, 1,
+            {
+                width: '50%',
+            },
+            {
+                width: '100%',
+            }),0
+        )
+        //move border to the left
+        .add(TweenMax.fromTo(
+            $border, 1,
+            {
+                left: `50%`,
+            },
+            {
+                left: `0`,
+            }),0
+        )
+
+    const controller = new ScrollMagic.Controller()
+    const animScene = new ScrollMagic.Scene({
+      triggerElement: $triggerEl[0],
+      triggerHook: 0,
+      duration: '30%',
+      tweenChanges: true
+    })
+        .setTween(plateAnimation)
+        .addIndicators({name: "TESTING"})
+        .addTo(controller);
+}
+
 function homePage() {
+    doPlateSlider()
     productsSection()
     testimonialSection()
 }
