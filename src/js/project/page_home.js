98e43d2cd5a2b42149712955110e81d878c19527
jab <joseadrianbuctuanon95@gmail.com>
2021-01-14 03:19:22 +0800

    added testimonial down to footer

--- src/js/project/page_home.js
+++ src/js/project/page_home.js
@@ -1,7 +1,53 @@
-function homePage() {
-    $('.main-carousel').flickity({
-        // options
-        cellAlign: 'left',
-        contain: true
+function productsSection() {
+    const $productCarousel = $('.prod-carousel')
+    const $thumbsParent = $('.grid-x--thumbs')
+
+    const $prodSlide = $('.prod-slide')
+    const $thumbSlide = $('.thumb-slide')
+
+    $productCarousel.flickity({
+        imagesLoaded: true ,
+        adaptiveHeight: true ,
+        prevNextButtons: false ,
+        pageDots: false
+    })
+
+
+    $productCarousel.on( 'change.flickity', function( event, index ) {
+        console.log( 'Slide changed to ' + index )
+
+        const $recentActive = $thumbsParent.find('.selected')
+        const $newActive = $thumbsParent.find(`[data-indx="${index}"]`)
+
+        $recentActive.removeClass('selected')
+        $newActive.addClass('selected')
+
     });
+
+    $thumbSlide.click(function() {
+        const $currThumb = $(this)
+        const { indx } = $currThumb.data()
+        const $recentActive = $thumbsParent.find('.selected')
+
+        console.log('indx === ', indx)
+        $productCarousel.flickity( 'select', indx );
+        $recentActive.removeClass('selected')
+        $currThumb.addClass('selected')
+    })
+}
+
+function testimonialSection() {
+    const $testimonialCarousel = $('.testimonial-carousel')
+
+    $testimonialCarousel.flickity({
+        imagesLoaded: true ,
+        adaptiveHeight: true ,
+        prevNextButtons: false ,
+        pageDots: false
+    })
+}
+
+function homePage() {
+    productsSection()
+    testimonialSection()
 }
